0b69f74710eb937745e95e285009d4ef4422b248
Norin Stark <33775952+NorinStark@users.noreply.github.com>
2019-07-25 00:46:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+import json
 import os
 import sys
-import json
-import requests
+from Utils import MessengerAPI
 
 from pymongo import MongoClient
-from flask import Flask, request, render_template
+from flask import Flask, request
+
+from Utils.MessengerAPI import send_message
 
 app = Flask(__name__)
 
+#Setting Environment for MongoDB
 MONGO_URI = 'mongodb://NorinStark:123456Nr@ds251877.mlab.com:51877/heroku_jqdp5fwm'
-client = MongoClient(MONGO_URI)
-messages = client["heroku_jqdp5fwm"] #database
+mongo = MongoClient(MONGO_URI)
+db = mongo["heroku_jqdp5fwm"]  #connect with the database's name
+
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -23,20 +27,15 @@ def verify():
 
     return "Hello world", 200
 
-@app.route('/index')
-def index():
-    return render_template("index.html")
-
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-   return 'Blog Number %d' % postID
+# @app.route('/index')
+# def index():
+#     return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def webhook():
 
     # endpoint for processing incoming messaging events
     try:
-
         data = request.get_json()
         print(data)
 
@@ -51,56 +50,67 @@ def webhook():
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
 
-                        # msg = messages["first1234"]  # colelction
-                        # message = msg.find({"keyword": message_text})
-                        # print('here')
-                        # print(message)
-                        # for thing in message:
-                        #     print('========>',thing['respone'])
-                        #
-                        # response = thing['respone']
+                        msg = db["first1234"]
+                        memo = db["memory"]
+                        message = msg.find({"keyword": message_text})
+                        print(message_text)
 
-                        # response = {"message" : {
-                        #         "attachment":{
-                        #           "type":"template",
-                        #           "payload":{
-                        #             "template_type":"generic",
-                        #             "elements":[
-                        #                {
-                        #                 "title":"Welcome!",
-                        #                 "image_url":"",
-                        #                 "subtitle":"The true is... I am iron man!",
-                        #                 "default_action": {
-                        #                   "type": "web_url",
-                        #                   "url": "https://google.com",
-                        #                   "webview_height_ratio": "tall",
-                        #                 },
-                        #                 "buttons":[
-                        #                   {
-                        #                     "type":"web_url",
-                        #                     "url":"https://google.com",
-                        #                     "title":"View Website"
-                        #                   },{
-                        #                     "type":"postback",
-                        #                     "title":"Start Chatting",
-                        #                     "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                        #                   }
-                        #                 ]
-                        #               }
-                        #             ]
-                        #           }
-                        #         }
-                        #       }
-                        # }
-                        #
-                        #
-                        send_message(sender_id)
+                        try:
+                            try:
+                                        is_user_memo = memo.find({"sender_id": sender_id})
+                                        print('erererer')
+                                        for item in is_user_memo:
+                                            for key, value in item.items():
+                                                if value == 'null':
+                                                    response = key
+                                                    memo.update_one({"sender_id": sender_id}, {"$set": {key: message_text}})
+                                                    break
 
-                    if messaging_event.get("delivery"):  # delivery confirmation
+                                        message = msg.find({"keyword": response})
+                                        for msg in message:
+                                            output = msg['response']
+                                        send_message(sender_id, output)
+                                        print('finish')
+
+                            except Exception as e:
+                                        print('memo failed')
+                                        print(e)
+
+                            for items in message:
+                                        response = items["response"]
+
+                            if items["type"] == "memory":
+                                        memo.insert({"sender_id": sender_id})
+
+                                        for keyword in response:
+                                            memo.update({"sender_id": sender_id}, {"$set": {keyword: "null"}})
+                                        null_keyword = memo.find({"sender_id": sender_id})
+                                        print("0000>")
+
+                                        for item in null_keyword:
+                                            for key, value in item.items():
+                                                if value == 'null':
+                                                    response = key
+                                                    break
+                                        print('We are looking for ', response)
+                                        message = msg.find({"keyword": response})
+                                        for msg in message:
+                                            output = msg['response']
+                                        send_message(sender_id, output)
+                                        print('finish')
+
+                        except Exception as e:
+                            print(e)
+
+                        # send_message(sender_id, response)
+
+
+                    elif messaging_event.get("delivery"):  # delivery confirmation
                         pass
-                    if messaging_event.get("optin"):  # optin confirmation
+                    elif messaging_event.get("optin"):  # optin confirmation
                         pass
-                    if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    elif messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+
                         pass
 
         return "ok", 200
@@ -109,60 +119,6 @@ def webhook():
         return "ok", 200
         print(e)
 
-def send_message(recipient_id):
-
-    params = {
-        "access_token=": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment":{
-              "type":"template",
-              "payload":{
-                "template_type":"generic",
-                "elements":[
-                   {
-                    "title":"Welcome!",
-                    "image_url":"https://wallpapercave.com/iron-man-4k-wallpapers",
-                    "subtitle":"I am iron man!",
-                    "default_action": {
-                      "type": "web_url",
-                      "url": "https://google.com",
-                      "webview_height_ratio": "tall",
-                    },
-                    "buttons":[
-                      {
-                        "type":"web_url",
-                        "url":"https://morning-headland-59802.herokuapp.com/index",
-                        "title":"View Website",
-                        "webview_height_ratio": "compact",
-                        "messenger_extensions" : True
-                      },{
-                         "type": "web_url",
-                         "url": "https://google.com",
-                         "title": "View Website",
-                         "webview_height_ratio": "tall"
-                      },{
-                        "type":"postback",
-                        "title":"Start Chatting",
-                        "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                      }
-                    ]
-                  }
-                ]
-              }
-            }
-        }
-    })
-
-    vvv = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=EAAiXW5BCIMgBAKGspMXEuOdxtwHIZCTGOOfGNwnpE5CnFYj7ZAbzRhojkx0yAMS7Ca9lrQ64HuZAlfebs5nlKHJg9IQ0106hZBm7J5DxXN7jRoiieeRkwHARw8fNznyuXrteKnSW7dIWarLZCcFQaGfQ2vbFYJ9ZAMZBSr9SSnquwbuX7AZBbvQnFkCyT4MwVIcZD", params=params, headers=headers, data=data)
-    print(vvv.text)
     return "ok", 200
 if __name__ == '__main__':
     app.run(DEBUG=True)
